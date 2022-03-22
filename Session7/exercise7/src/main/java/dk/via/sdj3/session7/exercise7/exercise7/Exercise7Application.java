af00367e2019e2dd360b291a2e2f795e67eb8362
christopher gadgaard <80517220+Chris309283@users.noreply.github.com>
2022-03-22 11:26:12 +0100

    start
    start

---
+++ Session7/exercise7/src/main/java/dk/via/sdj3/session7/exercise7/exercise7/Exercise7Application.java
@@ -0,0 +1,14 @@
+package dk.via.sdj3.session7.exercise7.exercise7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class Exercise7Application
+{
+
+  public static void main(String[] args)
+  {
+    SpringApplication.run(Exercise7Application.class, args);
+  }
+
+}
---
+++ Session7/exercise7/src/test/java/dk/via/sdj3/session7/exercise7/exercise7/Exercise7ApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.via.sdj3.session7.exercise7.exercise7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class Exercise7ApplicationTests
+{
+
+  @Test void contextLoads()
+  {
+  }
+
+}
